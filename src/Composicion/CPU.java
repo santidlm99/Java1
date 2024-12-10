65b1a90e40daec5d8a1569fbe9ff362166f81bd1
nombre <email@gmail.com>
2024-12-10 13:51:46 +0100

    h

--- src/Composicion/CPU.java
+++ src/Composicion/CPU.java
@@ -2,39 +2,32 @@ package Composicion;
 
 public class CPU {
 
-    private String marca;
-    private String procesador;
-    private String precio;
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getProcesador() {
-        return procesador;
-    }
-
-    public void setProcesador(String procesador) {
-        this.procesador = procesador;
-    }
-
-    public CPU(String marca, String procesador, String precio) {
-        this.marca = marca;
-        this.procesador = procesador;
-        this.precio = precio;
-    }
-
-    public String getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(String precio) {
-        this.precio = precio;
-    }
-
-    public String getMarca() {
-
-        return marca;
-    
-    }
+	private String marca;
+	private String procesador;
+	private double precio;
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getProcesador() {
+		return procesador;
+	}
+	public void setProcesador(String procesador) {
+		this.procesador = procesador;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public CPU(String marca, String procesador, double precio) {
+		super();
+		this.marca = marca;
+		this.procesador = procesador;
+		this.precio = precio;
+	}
+	
 }
--- src/Composicion/principal01.java
+++ src/Composicion/principal01.java
@@ -2,12 +2,23 @@ package Composicion;
 
 public class principal01 {
 
-    public static void main(String[] args) {
+  
+	public static void main(String[] args) {
+		
+		Monitor m1= new Monitor("lg",200);
+		System.out.println(m1.getMarca());
+		System.out.println(m1.getPrecio());
+		
+		CPU cpu1= new CPU("ibm","intel",500);
+		System.out.println(cpu1.getMarca());
+		System.out.println(cpu1.getProcesador());
+		System.out.println(cpu1.getPrecio());
+		
+		Ordenador o= new Ordenador(m1,cpu1);
 
-        Monitor m1= new Monitor("lg",200);
-        System.out.println(m1.getMarca());
-        System.out.println(m1.getPrecio());
+        System.out.println(o.preciototal());
+		
 
-    }
+	}
 
 }
